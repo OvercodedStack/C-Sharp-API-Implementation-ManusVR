e346f63c68649de11268b98e9d4c2ea5493aa818
OvercodedStack <vesperiumcreator@gmail.com>
2018-06-26 16:46:33 -0400

    Tuesday Updates

--- Utillity Scripts/Camera_controls.cs
+++ Utillity Scripts/Camera_controls.cs
@@ -80,7 +80,7 @@ public class Camera_controls : MonoBehaviour
                 float ScrollAmount = Input.GetAxis("Mouse ScrollWheel") * ScrollSensitvity;
                 ScrollAmount *= (this._CameraDistance * 0.3f);
                 this._CameraDistance += ScrollAmount * -1f;
-                this._CameraDistance = Mathf.Clamp(this._CameraDistance, 1.5f, 100f);
+                this._CameraDistance = Mathf.Clamp(this._CameraDistance, 0.5f, 100f);
             }
 
             //Actual Camera Rig Transformations
--- Utillity Scripts/Modify_my_slider.cs
+++ Utillity Scripts/Modify_my_slider.cs
@@ -21,30 +21,36 @@ public class Modify_my_slider : MonoBehaviour {
     private Slider[] sliders_on_gameObj;
     private Manus_interpreter manus_inpt;
     private float z1, z2, z3, z4, z5; // Motor movement overrides
-	
+    public Toggle toggle_manus;
+
     //Initialization
-	void Start () {
+    void Start () {
         sliders_on_gameObj = this.GetComponentsInChildren<Slider>();    //Get my components
         //manus_inpt = this.GetComponent<Manus_interpreter>();            //Get my manus API
         GameObject game_thing = GameObject.Find("Manus_VR_Driver");     //Seek global API
         manus_inpt = game_thing.GetComponent<Manus_interpreter>();
+       
     }
 
-	void Update () {
-        Manus_hand_obj hand = manus_inpt.get_hand(device_type_t.GLOVE_RIGHT);   //Select glove
-        double[] hand_compression_ratios = hand.get_raw_hand().ToArray();       //Array conversion
-        z1 = (float)hand_compression_ratios[6]; //Index
-        z2 = (float)hand_compression_ratios[4]; //Middle
-        z3 = (float)hand_compression_ratios[2]; //Ring
-        z4 = (float)hand_compression_ratios[0]; //Pinky
-        z5 = (float)hand_compression_ratios[8]; //Thumb
+    void Update()
+    {
+        if (toggle_manus.isOn)
+        {
+            Manus_hand_obj hand = manus_inpt.get_hand(device_type_t.GLOVE_RIGHT);   //Select glove
+            double[] hand_compression_ratios = hand.get_raw_hand().ToArray();       //Array conversion
+            z1 = (float)hand_compression_ratios[6]; //Index
+            z2 = (float)hand_compression_ratios[4]; //Middle
+            z3 = (float)hand_compression_ratios[2]; //Ring
+            z4 = (float)hand_compression_ratios[0]; //Pinky
+            z5 = (float)hand_compression_ratios[8]; //Thumb
 
-        //Slider Overrides
-        sliders_on_gameObj[0].value = z5 * 111F - 85.0F;    //Rotate the base
-        sliders_on_gameObj[1].value = z1 * 350.0F - 175.0F; //Move the second arm
-        sliders_on_gameObj[2].value = z2 * 310.0F - 155.0F; //Move the first arm
-        sliders_on_gameObj[3].value = z3 * 690.0F - 345.0F; //Rotate the last arm
-        sliders_on_gameObj[4].value = 0;                    //???
-        ///sliders_on_gameObj[5] exists but useless for now. 
+            //Slider Overrides
+            sliders_on_gameObj[0].value = z5 * 111F - 85.0F;    //Rotate the base
+            sliders_on_gameObj[1].value = z1 * 350.0F - 175.0F; //Move the second arm
+            sliders_on_gameObj[2].value = z2 * 310.0F - 155.0F; //Move the first arm
+            sliders_on_gameObj[3].value = z3 * 690.0F - 345.0F; //Rotate the last arm
+            sliders_on_gameObj[4].value = 0;                    //???
+                                                                ///sliders_on_gameObj[5] exists but useless for now. 
+        }
     }
 }
