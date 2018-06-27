d78723837bf17dc18abfec22537cb5550ff8509b
OvercodedStack <vesperiumcreator@gmail.com>
2018-06-27 16:42:11 -0400

    Wednesday Update

---
+++ Utillity Scripts/UR5_transmission_serivce.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UR5_transmission_serivce : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
